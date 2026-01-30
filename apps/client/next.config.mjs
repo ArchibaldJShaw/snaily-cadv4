c8454495f0d2fcf945e24bd192e8262c8d9ea666
archibaldjshaw <archibaldjshaw@users.noreply.github.com>
2026-01-30 21:24:45 +0200

    Remove Sentry integration completely

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import { i18n } from "./i18n.config.mjs";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
@@ -42,58 +41,6 @@ const nextConfig = {
       },
     ]
   }, // end images
-  // prettier-enable
-  webpack(config, { webpack }) {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        __SENTRY_DEBUG__: false,
-      }),
-    );
-    return config;
-  },
-  sentry: {
-    hideSourceMaps: false,
-    widenClientFileUpload: true,
-  },
 }; // end config
 
-/** @type {typeof nextConfig} */
-export default (phase, defaultConfig) => {
-  const plugins = [
-    (config) =>
-      withSentryConfig(
-        config,
-        {
-          org: "snailycad",
-          setCommits: true,
-          project: "snailycad-client",
-          authToken: "bce2b8a2e79f4336a3b115f171e675ab639d2d1f809a40919ca4493257a9c0e0",
-          release: json.version,
-          silent: true,
-          hideSourceMaps: false,
-          widenClientFileUpload: true,
-        },
-        {
-          tunnelRoute: "/api/monitoring-tunnel",
-        },
-      ),
-  ];
-
-  const config = plugins.reduce(
-    (acc, plugin) => {
-      const pluginReturnValue = plugin(acc);
-
-      let newConfig;
-      if (typeof pluginReturnValue === "function") {
-        newConfig = pluginReturnValue(phase, defaultConfig);
-      } else {
-        newConfig = pluginReturnValue;
-      }
-
-      return newConfig;
-    },
-    { ...nextConfig },
-  );
-
-  return config;
-};
+export default nextConfig;
